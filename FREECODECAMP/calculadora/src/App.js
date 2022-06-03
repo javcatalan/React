c2159768db4aa3543135a0f6a3c03c559235376f
javcatalan <javclla@gmail.com>
2022-06-03 17:02:14 -0600

    calculadora

---
+++ FREECODECAMP/calculadora/src/App.js
@@ -0,0 +1,73 @@
+import './App.css';
+import freeCodeCampLogo from './imagenes/python-and-finance-af7335a89d21d5d0fd49fce5cc9d187c.png'
+import Boton from './componentes/Boton';
+import Pantalla from './componentes/Pantalla';
+import BotonClear from './componentes/BotonClear';
+import { useState } from 'react';
+import { evaluate } from 'mathjs';
+
+function App() {
+
+  const [input, setInput] = useState('');
+
+  const agregarInput = val => {
+    setInput(input + val);
+  };
+
+  const calcularResultado = () => {
+    if (input){
+      setInput(evaluate(input));
+    }
+    else
+      alert('ingrese valores primero para realizar calculos')
+    
+
+  };
+
+  return (
+    <div className="App">
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+        src={freeCodeCampLogo}
+        className='freecodecamp-logo'
+        alt='logo de freecodecamp'
+        />
+      </div>
+      <div className='contenedor-calculadora'>
+        <Pantalla input={input}/>
+        <div className='fila'>
+          <Boton manejarClic={agregarInput}>1</Boton>
+          <Boton manejarClic={agregarInput}>2</Boton>
+          <Boton manejarClic={agregarInput}>3</Boton>
+          <Boton manejarClic={agregarInput}>+</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClic={agregarInput}>4</Boton>
+          <Boton manejarClic={agregarInput}>5</Boton>
+          <Boton manejarClic={agregarInput}>6</Boton>
+          <Boton manejarClic={agregarInput}>-</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClic={agregarInput}>7</Boton>
+          <Boton manejarClic={agregarInput}>8</Boton>
+          <Boton manejarClic={agregarInput}>9</Boton>
+          <Boton manejarClic={agregarInput}>*</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClic={calcularResultado}>=</Boton>
+          <Boton manejarClic={agregarInput}>0</Boton>
+          <Boton manejarClic={agregarInput}>.</Boton>
+          <Boton manejarClic={agregarInput}>/</Boton>
+        </div>
+        <div className='fila'>
+          <BotonClear manejarClear={() => setInput('')}>
+            Clear
+          </BotonClear>  
+        </div>        
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ FREECODECAMP/calculadora/src/componentes/Boton.js
@@ -0,0 +1,19 @@
+import React from "react";
+import '../hojas-estilos/Boton.css'
+
+function Boton (props) {
+
+  const esOperador = valor => {
+    return isNaN(valor) && (valor !== '.') && (valor !== '=')
+
+  }
+    return (
+      <button
+        className={`boton-contenedor ${esOperador(props.children) ? 'operador' : null}`}
+        onClick={() => props.manejarClic(props.children)}>
+        {props.children}
+      </button>
+    );
+}
+
+export default Boton
